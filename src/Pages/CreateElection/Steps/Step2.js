2e6a71da08e5f4b10c05d119136184f59b1c7758
fostersoasare <fostersoasare@gmail.com>
2023-02-08 12:14:12 +0000

    Validating category limits

--- src/Pages/CreateElection/Steps/Step2.js
+++ src/Pages/CreateElection/Steps/Step2.js
@@ -3,10 +3,26 @@ import CategoryPopup from "../CategoryPopup/CategoryPopup";
 import Category from "../Category/Category";
 import CandidatePopup from "../CandidatePopup/CandidatePopup";
 import { useElectionContext } from "../../../Context/ElectionContext";
+import Error from "../../../Components/form/Error";
+import { useAuthContext } from "../../../Context/AuthContext";
 
 const Step2 = () => {
 	const { electionData, showCategoryForm, showCandidateForm, setShowCategoryForm, prevStep, electionDataDispatchFunc } = useElectionContext();
+	const { error, errorDispatchFunc } = useAuthContext();
 
+	function previewElection() {
+		let limitPassed = electionData.data.categories.every((category) => {
+			return parseInt(category.limit) < category.candidates.length;
+		});
+		if (!limitPassed) {
+			errorDispatchFunc({ type: "displayError", payload: "Please make sure all categories have more candidates than the limit specified " });
+			setTimeout(() => {
+				errorDispatchFunc({ type: "clearError" });
+			}, 5000);
+			return;
+		}
+		electionDataDispatchFunc({ type: "nextStep" });
+	}
 	return (
 		<>
 			<section className="step2">
@@ -22,16 +38,14 @@ const Step2 = () => {
 					})}
 				</div>
 
+				{error.display === "block" && <Error text={error.text} />}
+
 				{electionData?.data?.categories?.length > 0 && (
 					<div className="actions">
 						<button className="button__secondary" onClick={prevStep}>
 							Prev
 						</button>
-						<button
-							className="button__primary"
-							onClick={() => {
-								electionDataDispatchFunc({ type: "nextStep" });
-							}}>
+						<button className="button__primary" onClick={previewElection}>
 							Preview Election
 						</button>
 					</div>
